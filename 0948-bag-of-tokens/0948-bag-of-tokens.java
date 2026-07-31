b58eb3aef9932017f56ed79cbde002025315a136
Mohammed Musavvir Ahmed <150333933+Md-musavvir@users.noreply.github.com>
2026-07-31 12:26:07 +0530

    Time: 7 ms (19.29%), Space: 45.3 MB (40.19%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.java
+++ 0948-bag-of-tokens/0948-bag-of-tokens.java
@@ -1,24 +1,22 @@
 class Solution {
     public int bagOfTokensScore(int[] tokens, int power) {
-        Arrays.sort(tokens);
-        int p=power;
         int res=0;
+        int max=0;
+        int p=power;
         int l=0;
         int r=tokens.length-1;
-        int max=0;
+        Arrays.sort(tokens);
         while(l<=r){
-            if(tokens[l]<=p){
+            if(p>=tokens[l]){
                 p-=tokens[l];
-                l++;
                 res++;
+                l++;
                 max=Math.max(res,max);
-
-
             }
             else if(res>=1){
                 p+=tokens[r];
-                r--;
                 res--;
+                r--;
             }
             else{
                 break;
